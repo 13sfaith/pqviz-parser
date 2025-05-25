994d4352b0f51bec21ce34308c31e16cf67bb502
Spencer Faith <13sfaith@gmail.com>
2025-05-24 21:05:15 -0700

    clean up

--- tests/traceParser.test.js
+++
@@ -1,7 +0,0 @@
-import { describe, it } from 'vitest';
-import { parseTrace } from '../src/parser/traceParser';
-describe('parseTrace', () => {
-    it('should print', () => {
-        parseTrace();
-    });
-});
